a2e074e2e9f8fd7142a50c4338c4b02f517de12d
Rafal Sleziak <226390@student.pwr.edu.pl>
2018-05-27 18:10:16 +0200

    Add project files.

---
+++ BazyDanych/Connection.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Windows.Forms;
+
+
+namespace BazyDanych
+{
+    class Connection
+    {
+        public static string MyConnectionString = "Server=localhost;Database=magazyn;port=3306;Uid=root;Pwd=bazydanych;SslMode=none";
+        
+        public DataSet LoadData()
+        {
+            MySqlConnection connection = new MySqlConnection(MyConnectionString);
+            connection.Open();
+            try
+            {
+                MySqlCommand cmd = connection.CreateCommand();
+                cmd.CommandText = "SELECT * FROM klient";
+                MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                DataSet dataSet = new DataSet();
+                adapter.Fill(dataSet);
+                return dataSet;
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+
+            if(connection.State==ConnectionState.Open)
+            {
+                connection.Clone();
+            }
+        }
+    }
+}
---
+++ BazyDanych/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace BazyDanych
+{
+    static class Program
+    {
+
+        
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+           // Connection connection= new Connection();
+            //connection.LoadData();
+        }
+    }
+}
